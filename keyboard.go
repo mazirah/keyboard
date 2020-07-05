6b4338572f06ef6737ad767a3b9badf0cd0e29a4
Simeon <66328487+silentpaw@users.noreply.github.com>
2020-07-05 09:54:11 +0300

    Add keyboard package
    A Go package for reading keyboard input

---
+++ keyboard.go
@@ -0,0 +1,24 @@
+package keyboard
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//GetFloat takes a string from Stdin trims it and coverts it float64, then returns it
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return number, nil
+}
