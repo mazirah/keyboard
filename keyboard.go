0586a3fc589a14e840032f2ade2c6c42f61b4410
Simeon <66328487+silentpaw@users.noreply.github.com>
2020-07-31 17:03:00 +0300

    added GetInt() function

--- keyboard.go
+++ keyboard.go
@@ -8,18 +8,31 @@ import (
 	"strconv"
 	"strings"
 )
+//Fast error checking
+func check(err error) {
+	if err !=nil {
+		log.Fatal(err)
+	}
+}
 
-//GetFloat reads a number from the keyboard. It returns the number and any error encountered
+//GetFloat reads a float number from the keyboard. It returns the number and any error encountered
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	input = strings.TrimSpace(input)
 	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
+	return number, nil
+}
+
+//GetInt reads an int number from the keyboard. It returns the number and any error encountered
+func GetInt() (int, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	check(err)
+	input = strings.TrimSpace(input)
+	number, err := strconv.Atoi(input)
+	check(err)
 	return number, nil
 }
